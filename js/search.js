543109db1f3ee9a08ed996923c5ac39349e95f21
Donavan Armstrong <donavan.mc@gmail.com>
2018-11-29 16:34:43 -0800

    photo gallery

--- js/search.js
+++ js/search.js
@@ -7,7 +7,7 @@ function searchBar() {
 
     for (i = 0; i < pics.length; i++) {
         info = pics[i].getElementsByTagName("a")[0];
-        if (info.innerHTML.toLowerCase().indexOf(style) > -1) {
+        if (info.getAttribute('data-title').toLowerCase().indexOf(style) > -1) {
             pics[i].style.display = "";
         } else {
             pics[i].style.display = "none";
